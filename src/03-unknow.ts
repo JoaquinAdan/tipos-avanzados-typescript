97ddbabd69936591c4ada25f58677b1f4e829ff9
JoaquinAdan <rodriguezjdev@gmail.com>
2023-05-08 15:59:42 -0300

    unknows

--- src/03-unknow.ts
+++ src/03-unknow.ts
@@ -4,9 +4,9 @@ anyVar = undefined;
 anyVar = null;
 anyVar = 1;
 anyVar = [];
-anyVar = {};
-
-let isNew: boolean = anyVar;
+anyVar = {}; //⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅⬅ ⬅ ⬅ ⬅ ⬅ ⬅ ⬆
+//                                                         ⬆
+let isNew: boolean = anyVar; // ultimo valor fue un objeto ⬆
 
 let unknownVar: unknown;
 unknownVar = true;
