11b4e276d622a9bf9c89c9ea9608f44feb1b447e
JoaquinAdan <joacorodriguez.98l@gmail.com>
2023-05-10 21:06:22 -0300

    rest operator in ts

--- src/07-rest.ts
+++ src/07-rest.ts
@@ -8,10 +8,46 @@ const currentUser: User = {
 export const checkAdminRole = () => {
   // esto es un guardian
   if (currentUser.role === ROLES.ADMIN) {
-    console.log('es admin');
+    return 'es admin';
   } else {
-    console.log('no es admin');
+    return 'no es admin';
   }
 };
 
-checkAdminRole();
+const rta = checkAdminRole();
+console.log(`${currentUser.username} ${rta}`);
+
+export const checkRole = (role1: string, role2: string) => {
+  if (currentUser.role === role1) {
+    return 'es customer';
+  }
+  if (currentUser.role === role2) {
+    return 'es seller';
+  }
+  return 'es admin';
+};
+
+const rta2 = checkRole(ROLES.CUSTOMER, ROLES.SELLER);
+console.log('CheckRole', rta2);
+
+//
+export const checkRoleV2 = (roles: string[]) => {
+  if (roles.includes(currentUser.role)) {
+    return 'es customer';
+  }
+  return 'es admin';
+};
+
+const rta3 = checkRoleV2([ROLES.ADMIN, ROLES.SELLER]);
+console.log('CheckRoleV2', rta3);
+
+//
+export const checkRoleV3 = (...roles: string[]) => {
+  if (roles.includes(currentUser.role)) {
+    return true;
+  }
+  return false;
+};
+
+const rta4 = checkRoleV3(ROLES.ADMIN, ROLES.SELLER, ROLES.CUSTOMER);
+console.log('CheckRoleV3', rta4);
