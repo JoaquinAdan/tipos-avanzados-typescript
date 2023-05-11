db0e2686a632f51345e9a08a1418d6580efe4314
JoaquinAdan <rodriguezjdev@gmail.com>
2023-05-11 14:59:52 -0300

    ts

---
+++ src/08-overload.ts
@@ -0,0 +1,21 @@
+// Joaquin => [J,o,a,q,u,i,n] => string => string[]
+// [J,o,a,q,u,i,n] => Joaquin => string[] => string
+
+function parseStr(input: string | string[]): string | string[] {
+  if (Array.isArray(input)) {
+    return input.join(''); // string
+  } else {
+    return input.split(''); // string[]
+  }
+}
+
+const rtaArray = parseStr('Joaquin');
+if (Array.isArray(rtaArray)) {
+  rtaArray.reverse();
+}
+console.log('Joaquin => ', rtaArray);
+
+const rtaString = parseStr(['J', 'o', 'a', 'q', 'u', 'i', 'n']);
+if (typeof rtaString === 'string') {
+  console.log(rtaString.toUpperCase());
+}
